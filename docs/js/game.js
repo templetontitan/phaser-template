605e211050c7d617bc726e319b8c7ae802b3f0df
janzeteachesit <janzeteachesit@gmail.com>
2017-04-07 16:27:05 -0700

    Update game.js

--- docs/js/game.js
+++ docs/js/game.js
@@ -6,22 +6,26 @@ var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, 'hello-world-example', {
   create: create, 
   update: update}
   );
+
 function preload() {
-game.load.image('background', 'assets/images/sky.png');game.load.image('bubble-img', 'assets/images/bubble.png');}
+	game.load.image('background', 'assets/images/sky.png');
+	game.load.image('bubble-img', 'assets/images/bubble.png');
+}
 function create() {
-game.physics.startSystem(Phaser.Physics.ARCADE);
-background = game.add.sprite(0, 0, 'background');
-			background.scale.setTo(4,4);
-			
-			bubble = game.add.sprite(WIDTH / 2, 0, 'bubble-img');
-			bubble.scale.setTo(0.3, 0.3);
-			
-			game.physics.enable(bubble, Phaser.Physics.ARCADE);
-			bubble.body.gravity.y = 200;
-			bubble.body.collideWorldBounds = true;
-			bubble.body.bounce.set(1);
-			
-			bubble.body.velocity.setTo(200,200);
-		}
-		function update() {
-		}
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+	
+	background = game.add.sprite(0, 0, 'background');
+	background.scale.setTo(4,4);
+	
+	bubble = game.add.sprite(WIDTH / 2, 0, 'bubble-img');
+	bubble.scale.setTo(0.3, 0.3);
+	
+	game.physics.enable(bubble, Phaser.Physics.ARCADE);
+	bubble.body.gravity.y = 200;
+	bubble.body.collideWorldBounds = true;
+	bubble.body.bounce.set(1);
+	
+	bubble.body.velocity.setTo(200,200);
+}
+function update() {
+}
